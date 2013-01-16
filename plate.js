74024db634a0dc6b0c421d81dd4cd38e5ce3981f
Pete Romano <promano@urbandaddy.com>
2013-01-16 16:28:02 -0500

    Added support for load tag (left unimplemented; it just doesn't fail)

--- plate.js
+++ plate.js
@@ -1036,6 +1036,7 @@ proto.builtins = {
   , 'for': require('./tags/for').parse
   , 'if': require('./tags/if/node').parse
   , 'include': require('./tags/include').parse
+  , 'load': require('./tags/load').parse
   , 'now': require('./tags/now').parse
   , 'with': require('./tags/with').parse
 }
@@ -1952,6 +1953,73 @@ proto.render = function(context, target) {
   promise = new Promise
 
   target.render(context, function(err, data) {
+    promise.resolve(data)
+  })
+
+  return promise
+}
+
+proto.get_template = function(target) {
+  if(typeof target === 'string') {
+    return this.loader(target)
+  }
+
+  // okay, it's probably a template object
+  return target
+}
+});
+
+require.define("/lib/tags/load.js",function(require,module,exports,__dirname,__filename,process){module.exports = LoadNode
+
+var Promise = require('../promise')
+
+function LoadNode(target_var, loader) {
+  this.target_var = target_var
+  this.loader = loader
+}
+
+var cons = LoadNode
+  , proto = cons.prototype
+
+cons.parse = function(contents, parser) {
+  var bits = contents.split(' ')
+    , varname = parser.compile(bits.slice(1).join(' '))
+    , loader = parser.plugins.lookup('loader')
+
+  return new cons(varname, loader)
+}
+
+proto.render = function(context, target) {
+  var self = this
+    , promise
+
+  target = target || this.target_var.resolve(context)
+
+  if(target.constructor === Promise) {
+    promise = new Promise
+
+    target.once('done', function(data) {
+      promise.resolve(self.render(context, data))
+    })
+
+    return promise
+  }
+
+  target = self.get_template(target)
+
+  if(target.constructor === Promise) {
+    promise = new Promise
+
+    target.once('done', function(data) {
+      promise.resolve(self.render(context, data))
+    })
+
+    return promise
+  }
+
+  promise = new Promise
+
+  target.render(context.copy(), function(err, data) {
     promise.resolve(data)
   })
 
